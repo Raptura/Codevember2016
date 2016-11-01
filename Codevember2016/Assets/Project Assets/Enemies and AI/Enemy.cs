94cf132957beceab58bab861ffe775d75c387e20
Armond Smith <smithark97@gmail.com>
2016-11-01 19:08:33 -0400

    Reprocessed the Repo

---
+++ Codevember2016/Assets/Project Assets/Enemies and AI/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Enemy : CombatEntity {
+
+    public override void processDamage(int dmg)
+    {
+        health -= dmg;
+
+        //TODO: Display Combat Text
+
+        if (health == 0)
+            die();
+    }
+
+
+    protected override int calculateDamage(CombatEntity target)
+    {
+        int rand = Random.Range(negRange, posRange);
+        int power = 0;
+        if (job == Job.Warrior || job == Job.Ranger)
+            power = ATK;
+        else if (job == Job.Mage)
+            power = SPE;
+
+        return power + rand - target.DEF;
+    }
+
+    public abstract void die();
+}
---
+++ Codevember2016/Assets/Project Assets/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Player : CombatEntity
+{
+
+
+    public override void processDamage(int dmg)
+    {
+        health -= dmg;
+
+        //TODO: Display Combat Text
+
+        if (health == 0)
+            die();
+    }
+
+    protected override int calculateDamage(CombatEntity target)
+    {
+        int rand = Random.Range(negRange, posRange);
+        int power = 0;
+        if (job == Job.Warrior || job == Job.Ranger)
+            power = ATK;
+        else if (job == Job.Mage)
+            power = SPE;
+
+        return power + rand - target.DEF;
+    }
+
+    protected void die()
+    {
+
+    }
+
+    void levelUp() {
+        //TODO: Base Stat increases based on random element and Job
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
