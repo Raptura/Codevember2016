a47527081fa45adb9c0fba6095042af869a1a20a
Armond Smith <smithark97@gmail.com>
2016-11-01 16:53:11 -0400

    Day One: Setup
    Created Player.cs
    Created Enemy.cs
    Created CombatEntity.cs
    
    Imported simple 2D Unity assets

---
+++ Codevember2016/Codevember2016/Assets/Project Assets/Combat Interaction/CombatEntity.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class CombatEntity : MonoBehaviour
+{
+
+    //Typical Stats
+    private int _health;
+    public int health
+    {
+        get { return _health; }
+        protected set
+        {
+            if (value <= 0)
+            {
+                _health = 0;
+            }
+            else if (value >= maxHealth)
+            {
+                _health = maxHealth;
+            }
+            else {
+                _health = value;
+            }
+        }
+    }
+    public int maxHealth { get; protected set; }
+    public int special { get; protected set; }
+    public int maxSpecial { get; protected set; }
+
+    public int level { get; protected set; }
+    public int ATK { get; protected set; }
+    public int SPE { get; protected set; }
+    public int RES { get; protected set; }
+    public int DEF { get; protected set; }
+
+    public enum Job
+    {
+        Warrior,
+        Mage,
+        Ranger
+    }
+    public Job job;
+
+    //Variables that are specific to the Skill Stat
+    protected int negRange { get; set; } //This is a negative value
+    protected int posRange { get; set; } //This is the positive value
+
+    public string SKILL { get { return negRange + " - " + posRange; } }
+
+    public abstract void processDamage(int dmg);
+
+    protected abstract int calculateDamage(CombatEntity target);
+
+}
